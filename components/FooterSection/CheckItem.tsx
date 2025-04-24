29eef314e685c0f38114c6242377fbef74f814e7
Alex-Shick <business@alexshick.com>
2025-04-23 22:55:00 -0400

    Reformatted the checkitem.tsx file.

--- components/FooterSection/CheckItem.tsx
+++ components/FooterSection/CheckItem.tsx
@@ -1,16 +1,9 @@
 import Image from "next/image";
-import React from "react";
 
-interface CheckItemProps {
-  text: string;
-}
-
-const CheckItem: React.FC<CheckItemProps> = ({ text }) => (
+const CheckItem = ({ text }: { text: string }) => (
   <div className="flex gap-3 items-center mb-4">
     <Image src="/assets/V.png" alt="Check" width={25} height={25} />
-    <div className="text-lg tracking-normal leading-none text-white">
-      {text}
-    </div>
+    <div className="text-lg tracking-normal leading-none text-white">{text}</div>
   </div>
 );
 
