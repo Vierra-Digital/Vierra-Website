f1d30cbc8fe3341f0ecb10f66dba0e27d7c88c81
Alex-Shick <business@alexshick.com>
2025-04-23 23:56:17 -0400

    Reformatted the utils in the library folder.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,4 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+export const cn = (...inputs: unknown[]) => twMerge(clsx(inputs));
