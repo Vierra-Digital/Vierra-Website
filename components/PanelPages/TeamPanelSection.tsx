ab64174733dd8dd2c322799565ee21f8dc57ebd0
ThomasWalsh1 <thomaswalsh217@gmail.com>
2025-08-29 15:00:42 -0400

    initial rework setup

---
+++ components/PanelPages/TeamPanelSection.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react"
+
+const TeamPanelSection = () => {
+    return (
+        <div className="w-full h-full flex items-center justify-center text-black">
+            <span>This is the Team section</span>
+        </div>
+    )
+}
+export default TeamPanelSection;
