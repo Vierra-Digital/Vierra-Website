2b036c8bb0eed6e22b09a861482dc8e7bcd6b874
ThomasWalsh1 <thomaswalsh217@gmail.com>
2025-09-08 13:43:21 -0400

    unused var fix

--- components/PanelPages/ClientsSection.tsx
+++ components/PanelPages/ClientsSection.tsx
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react"
-
 const ClientsSection = () => {
     return (
         <div className="w-full h-full flex items-center justify-center text-black">
--- components/PanelPages/TeamPanelSection.tsx
+++ components/PanelPages/TeamPanelSection.tsx
@@ -1,5 +1,3 @@
-import React, { useState, useEffect } from "react"
-
 const TeamPanelSection = () => {
     return (
         <div className="w-full h-full flex items-center justify-center text-black">
