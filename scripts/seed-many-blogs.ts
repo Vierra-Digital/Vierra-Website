0dcf2ef163c6e548ef0c79a5adb77fc50580cccf
Alex Shick <business@alexshick.com>
2025-10-15 22:48:15 -0400

    Updated ESLinting errors again.

--- scripts/seed-many-blogs.ts
+++ scripts/seed-many-blogs.ts
@@ -17,6 +17,7 @@ async function main() {
         data: {
             author_id: author.id,
             title: 'Innovation-driven Technology in The Workplace: A Guide 2.0',
+            description: 'A comprehensive guide to implementing innovation-driven technology solutions in modern workplaces.',
             content: `
             
             Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
@@ -30,7 +31,6 @@ Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapi
 Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
             
             `,
-            image_url: 'https://moriondocoffee.com/cdn/shop/articles/2_5f7d6839-b528-4473-a395-1393d4291578.jpg?v=1681190541',
             slug: 'innovation-driven-technology-in-the-workplace-a-guide-2',
             tag: "Insights",
             is_test: true
