dd1860389226e1d38df740ee028ef0d81aa099b0
Alex Shick <business@alexshick.com>
2026-02-23 12:41:52 -0500

    Removed crawling on assets.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -18,12 +18,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/blog/rss.xml`,
-      lastModified: now,
-      changeFrequency: 'daily',
-      priority: 0.4,
-    },
-    {
       url: `${baseUrl}/privacy-policy`,
       lastModified: now,
       changeFrequency: 'yearly',
