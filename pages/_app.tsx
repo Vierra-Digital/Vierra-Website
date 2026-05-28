ea81cf3bbaa61a2c9f529ee3057483106f38c46e
Alex Shick <business@alexshick.com>
2026-05-27 22:58:15 -0400

    Added Google tag analytics.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../app/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
@@ -11,6 +12,18 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
           <link rel="icon" href="/favicon.ico" />
           <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-65W30MKNXT"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-65W30MKNXT');
+          `}
+        </Script>
         <Component {...pageProps} />
       </>
     </SessionProvider>
