df1720253735717cc503cba02cba62fb1d104081
Alex Shick <business@alexshick.com>
2026-05-27 23:03:05 -0400

    Added Google header detections.

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ const geistMono = Geist_Mono({
 
 const SITE_URL = "https://vierradev.com"
 const META_IMAGE_URL = `${SITE_URL}/assets/meta-banner.png`
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || "G-65W30MKNXT"
 
 export const metadata = {
   metadataBase: new URL(SITE_URL),
@@ -216,6 +217,18 @@ export default function RootLayout({
         />
       </head>
       <body>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga4-init-app-router" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
         <RootLayoutClient
           geistSansVariable={geistSans.variable}
           geistMonoVariable={geistMono.variable}
